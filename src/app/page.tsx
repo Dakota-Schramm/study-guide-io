26c59be7dde1228f6363826fc9323371eb908382
Dakota Schramm <william.schramm11@gmail.com>
2024-01-25 16:13:48 -0600

    Test format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 export default function Home() {
   return (
     <main className="flex text-white w-full h-full justify-center items-between space-x-8">
-      <button className="border border-white h-24 lg:h-32 p-4">STEM</button>
-      <button className="border border-white h-24 lg:h-32 p-4">Writing</button>
+      <button type="button" className="border border-white h-24 lg:h-32 p-4">
+        STEM
+      </button>
+      <button type="button" className="border border-white h-24 lg:h-32 p-4">
+        Writing
+      </button>
     </main>
-  )
+  );
 }
