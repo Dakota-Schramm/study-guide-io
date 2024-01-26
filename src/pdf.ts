36f362f6b2e672bc950f08a16c85527e63614dc4
Dakota Schramm <william.schramm11@gmail.com>
2024-01-26 12:35:00 -0600

    Add remaining files for first pdf pr

---
+++ src/pdf.ts
@@ -0,0 +1,80 @@
+import { PDFDocument, StandardFonts, rgb, degrees,  } from 'pdf-lib'
+
+async function createPdf() {
+  const pdfDoc = await PDFDocument.create()
+  const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman)
+
+  const page = pdfDoc.addPage()
+  const { width, height } = page.getSize()
+  const fontSize = 30
+  page.drawText('Creating PDFs in JavaScript is awesome!', {
+    x: 50,
+    y: height - 4 * fontSize,
+    size: fontSize,
+    font: timesRomanFont,
+    color: rgb(0, 0.53, 0.71),
+  })
+
+  const pdfBytes = await pdfDoc.save()
+}
+
+async function modifyPdf() {
+  const url = 'https://pdf-lib.js.org/assets/with_update_sections.pdf'
+  const existingPdfBytes = await fetch(url).then(res => res.arrayBuffer())
+
+  const pdfDoc = await PDFDocument.load(existingPdfBytes)
+  const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica)
+
+  const pages = pdfDoc.getPages()
+  const firstPage = pages[0]
+  const { width, height } = firstPage.getSize()
+  firstPage.drawText('This text was added with JavaScript!', {
+    x: 5,
+    y: height / 2 + 300,
+    size: 50,
+    font: helveticaFont,
+    color: rgb(0.95, 0.1, 0.1),
+    rotate: degrees(-45),
+  })
+
+  const pdfBytes = await pdfDoc.save()
+}
+
+async function embedImages() {
+  const jpgUrl = 'https://pdf-lib.js.org/assets/cat_riding_unicorn.jpg'
+  const pngUrl = 'https://pdf-lib.js.org/assets/minions_banana_alpha.png'
+
+  const jpgImageBytes = await fetch(jpgUrl).then((res) => res.arrayBuffer())
+  const pngImageBytes = await fetch(pngUrl).then((res) => res.arrayBuffer())
+
+  const pdfDoc = await PDFDocument.create()
+
+  const jpgImage = await pdfDoc.embedJpg(jpgImageBytes)
+  const pngImage = await pdfDoc.embedPng(pngImageBytes)
+
+  const jpgDims = jpgImage.scale(0.5)
+  const pngDims = pngImage.scale(0.5)
+
+  const page = pdfDoc.addPage()
+
+  page.drawImage(jpgImage, {
+    x: page.getWidth() / 2 - jpgDims.width / 2,
+    y: page.getHeight() / 2 - jpgDims.height / 2 + 250,
+    width: jpgDims.width,
+    height: jpgDims.height,
+  })
+  page.drawImage(pngImage, {
+    x: page.getWidth() / 2 - pngDims.width / 2 + 75,
+    y: page.getHeight() / 2 - pngDims.height + 250,
+    width: pngDims.width,
+    height: pngDims.height,
+  })
+
+  const pdfBytes = await pdfDoc.save()
+}
+
+module.exports = {
+  createPdf,
+  modifyPdf,
+  embedImages,
+}
