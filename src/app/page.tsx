45a7b6205a16960135f3e1905ab418059ba07ba7
Dakota Schramm <william.schramm11@gmail.com>
2024-01-26 12:28:29 -0600

    Change main page to use links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="flex text-white w-full h-full justify-center items-between space-x-8">
-      <button type="button" className="border border-white h-24 lg:h-32 p-4">
+    <main className="flex w-full h-full justify-center items-between space-x-8">
+      <Link href="/stem" className="border h-24 lg:h-32 p-4">
         STEM
-      </button>
-      <button type="button" className="border border-white h-24 lg:h-32 p-4">
+      </Link>
+      <Link href="/writing" className="border h-24 lg:h-32 p-4">
         Writing
-      </button>
+      </Link>
     </main>
   );
 }
