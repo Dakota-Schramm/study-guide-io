53b40626402978037fd20acd2a3988913f3fdea0
Dakota Schramm <william.schramm11@gmail.com>
2024-01-31 17:35:37 -0600

    Extract methods for app setup

---
+++ src/user_lifecycle_methods.ts
@@ -0,0 +1,45 @@
+"use client";
+
+import { sitePath } from '@/utils';
+
+async function requestDirectoryPermission() {
+  try {
+    const fsdHandle = await window
+      .showDirectoryPicker({
+        mode: 'readwrite',
+        startIn: 'documents',
+      }) as FileSystemDirectoryHandle;
+
+    return fsdHandle
+  } catch (error: unknown) {
+    const exception = error as DOMException;
+    switch (exception.name) {
+      case 'AbortError': return null
+      default:           console.log(`${typeof exception}: ${exception.message}`)
+    }
+  }
+}
+
+export async function handleFileSetup(collectFileHandles) {
+  const fsdHandle = await requestDirectoryPermission();
+  if (!fsdHandle) {
+    // TODO: Fix message
+    alert('You must allow access to your file system to use this app.')
+    return;
+  }
+
+  console.log("Name => " + fsdHandle.name)
+
+  let files;
+  if (fsdHandle.name !== sitePath) {
+    const studyGuideIo = await fsdHandle.getDirectoryHandle( sitePath, {
+      create: true 
+    });
+    files = await Array.from(studyGuideIo.entries());
+  } else {
+    files = await Array.from(fsdHandle.entries());
+  }
+
+  collectFileHandles(files);
+  window.localStorage.setItem('isSetup', 'true');
+}
