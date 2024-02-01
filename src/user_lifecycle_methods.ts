9de69877641fb2fc39a4db52bed8d1b8cfe02acb
Dakota Schramm <william.schramm11@gmail.com>
2024-02-01 00:01:02 -0600

    Fix guard clause to exit function early

--- src/user_lifecycle_methods.ts
+++ src/user_lifecycle_methods.ts
@@ -1,39 +1,46 @@
 "use client";
 
-import { sitePath } from '@/lib/utils';
+import { sitePath } from "@/lib/utils";
 
-async function requestDirectoryPermission() {
+async function requestDirectoryPermission(userAction = true) {
+  if (!userAction) {
+    // TODO: Show an alert to prompt the user to allow access to their file system
+    return;
+  }
+
   try {
-    const fsdHandle = await window
-      .showDirectoryPicker({
-        mode: 'readwrite',
-        startIn: 'documents',
-      }) as FileSystemDirectoryHandle;
+    const fsdHandle = (await window.showDirectoryPicker({
+      mode: "readwrite",
+      startIn: "documents",
+    })) as FileSystemDirectoryHandle;
 
-    return fsdHandle
+    return fsdHandle;
   } catch (error: unknown) {
     const exception = error as DOMException;
     switch (exception.name) {
-      case 'AbortError': return null
-      default:           console.log(`${typeof exception}: ${exception.message}`)
+      case "AbortError":
+        return null;
+      default:
+        console.log(`${typeof exception}: ${exception.message}`);
     }
   }
 }
 
-export async function handleFileSetup(collectFileHandles) {
-  const fsdHandle = await requestDirectoryPermission();
+export async function handleFileSetup(
+  collectFileHandles: (files: FileSystemHandle[]) => void,
+  userAction = true,
+) {
+  const fsdHandle = await requestDirectoryPermission(userAction);
   if (!fsdHandle) {
     // TODO: Fix message
-    alert('You must allow access to your file system to use this app.')
+    alert("You must allow access to your file system to use this app.");
     return;
   }
 
-  console.log("Name => " + fsdHandle.name)
-
   let files;
   if (fsdHandle.name !== sitePath) {
-    const studyGuideIo = await fsdHandle.getDirectoryHandle( sitePath, {
-      create: true 
+    const studyGuideIo = await fsdHandle.getDirectoryHandle(sitePath, {
+      create: true,
     });
     files = await Array.from(studyGuideIo.entries());
   } else {
@@ -41,5 +48,5 @@ export async function handleFileSetup(collectFileHandles) {
   }
 
   collectFileHandles(files);
-  window.localStorage.setItem('isSetup', 'true');
+  window.localStorage.setItem("isSetup", "true");
 }
