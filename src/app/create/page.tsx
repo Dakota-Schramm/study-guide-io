286c30f99e6d1cb8d14d1bbd1c9b3a31471f5200
Dakota Schramm <william.schramm11@gmail.com>
2024-02-02 11:06:22 -0600

    Add redirect on create page

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,9 +1,16 @@
 "use client";
 
-import React from "react";
+import React, { useContext } from "react";
 import Link from "next/link";
+import { ProfessorContext } from "@/contexts/ProfessorContext";
 
 export default function CreateContent() {
+  const { professor } = useContext(ProfessorContext);
+
+  if (professor.stem === undefined) {
+    if (window) window.location.href = "/";
+  }
+
   return (
     <>
       <Link href="/stem" className="border h-24 lg:h-32 p-4">
