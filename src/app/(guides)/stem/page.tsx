4d69a7df4a4f62192bdc1c8277406ede44c9ec38
Dakota Schramm <william.schramm11@gmail.com>
2024-02-06 19:50:04 -0600

    Add redirect on stem page when config not initialized

--- src/app/(guides)/stem/page.tsx
+++ src/app/(guides)/stem/page.tsx
@@ -1,6 +1,17 @@
+"use client";
+
+import { useContext } from "react";
+
 import GuideCreationProcess from "../_guide_creation/GuideCreationProcess";
+import { UserContext } from "@/contexts/UserContext";
 
 const STEM = () => {
+  const { user } = useContext(UserContext);
+
+  if (user?.config === undefined) {
+    if (window) window.location.href = "/";
+  }
+
   return (
     <>
       <h1>STEM</h1>
