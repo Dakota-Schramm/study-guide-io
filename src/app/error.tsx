ec9ea0c40966429c464a980a19f2a15d80179239
Dakota Schramm <william.schramm11@gmail.com>
2024-02-09 17:15:36 -0600

    Moved HomeContent back into home page

---
+++ src/app/error.tsx
@@ -0,0 +1,59 @@
+'use client'
+
+import Link from 'next/link'
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+  if (error.message === "RestrictedAccessUserConfig::NotSupported") {
+    return <UnsupportedBrowserView />
+  } else if (error.message === "RestrictedAccessUserConfig::PermissionRejected") {
+    return <UserRefusedView />
+  }
+
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <button onClick={() => reset()}>Try again</button>
+    </div>
+  )
+}
+
+function UnsupportedBrowserView() {
+  return (
+    <>
+      <h2>
+        Sorry, but all of this app's features don't currently work on this
+        browser.
+      </h2>
+      <p>Consider switching to one of the following browsers instead:</p>
+      <ul>
+        <li>Google Chrome</li>
+        <li>Microsoft Edge</li>
+        <li>Opera</li>
+      </ul>
+    </>
+  );
+}
+
+function UserRefusedView() {
+  return (
+    <>
+      <h2>File System Permission has been Rejected:</h2>
+      <ul>
+        <li>App has limited features while in this mode</li>
+        <li>
+          This setting can be re-enabled at
+          <Link href="/settings">
+            <span>settings</span>
+          </Link>
+          .
+        </li>
+      </ul>
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,84 @@
 "use client";
 
-import React from "react";
+import React, { useContext } from "react";
+import Link from "next/link";
 
-import SettingsContent from "./settings/page";
-import { HomeContent } from "./_home/HomeContent";
-import CreateContent from "./create/page";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
+import { UserContext } from "@/contexts/UserContext";
+import { BaseCourse } from "../classes/course";
+import { sitePath } from "@/lib/utils";
+import { RestrictedAccessUserConfig } from "@/classes/config/user/restricted-access";
+import { useRouter } from "next/navigation";
+
+// TODO: Use OPFS as fallback if user says no to showing directory
 const Home = () => {
-  let index = "home";
-  const path = window.location.href;
-  if (path === "/settings") index = "settings";
-  else if (path === "/create") index = "create";
+  const { user } = useContext(UserContext);
+  const router = useRouter();
 
-  const content = {
-    home: <HomeContent />,
-    create: <CreateContent />,
-    settings: <SettingsContent />,
-  }[index];
+  const pageType = checkPageType(user.courses);
+
+  if (pageType === "personal") router.push("/courses");
 
   return (
     <main className="flex w-full h-full justify-center items-between space-x-8">
-      {content}
+      {pageType === "basic" && <BasicView />}
+      {pageType === "newUser" && <NewUserView />}
     </main>
   );
 };
+
+// TODO: Fix flashing of UserBouncerView that occurs on restricted users
+function NewUserView() {
+  const { user } = useContext(UserContext);
+
+  if (user.config instanceof RestrictedAccessUserConfig)
+    throw new Error("RestrictedAccessUserConfig::NotSupported")
+  if (user.config === null) 
+    throw new Error("RestrictedAccessUserConfig::PermissionRejected");
+
+  return <UserBouncerView />;
+}
+
+function UserBouncerView() {
+  const { user, reSyncCourses } = useContext(UserContext);
+
+  return (
+    <div className="flex flex-col space-y-8">
+      <div>Placeholder image...</div>
+      <h2>We need some things from you to get started...</h2>
+
+      <Alert className="bg-red-700 text-white">
+        <AlertTitle>Choosing Your Home Directory</AlertTitle>
+        <AlertDescription>
+          {`Please select either your ${sitePath} folder or the one it's in.`}
+        </AlertDescription>
+      </Alert>
+
+      <button type="button" onClick={() => reSyncCourses(user?.config)}>
+        Setup permissions
+      </button>
+    </div>
+  );
+}
+
+function BasicView() {
+  return (
+    <>
+      <Link href="/create">Create</Link>
+      <Link href="/settings">Settings</Link>
+    </>
+  );
+}
+
+function checkPageType(courses: BaseCourse[] | undefined) {
+  let index: "personal" | "basic" | "newUser";
+
+  if (typeof courses === "undefined") index = "newUser";
+  else if (courses.length) index = "personal";
+  else index = "basic";
+
+  return index;
+}
 
 export default Home;
