8c4ffb35fb4d8cd0af9990181156347c4197c3f1
Dakota Schramm <william.schramm11@gmail.com>
2024-02-10 18:17:32 -0600

    Download guide method used now from base config, works properly

--- src/classes/config/user/restricted-access.ts
+++ src/classes/config/user/restricted-access.ts
@@ -1,29 +1,8 @@
-import { openDB } from "idb";
-
-import { createPdf } from "@/app/(guides)/_guide_creation/pdf/createPdf";
-import { BaseUserConfig, DownloadGuideOptions } from "./base";
-import { getDatabaseInfo } from "@/lib/idbUtils";
-import { ensureError } from "@/lib/utils";
+import { BaseUserConfig } from "./base";
 
 export class RestrictedAccessUserConfig extends BaseUserConfig {
   async initialize() {
     const root = await navigator.storage.getDirectory();
     await super.initialize(root);
-  }
-
-  async downloadGuideToFileSystem(
-    pdfFiles: FileList,
-    attachmentFiles: FileList,
-    options: DownloadGuideOptions,
-  ): Promise<void> {
-    const db = await this.setupDatabaseWithStore("courses", options.courseName);
-
-    // TODO: Make sure type get set and put in db here
-
-    const pdfBlob = await createPdf(pdfFiles, attachmentFiles);
-    // const arrayBytes = await pdfBlob.arrayBuffer();
-
-    console.log(options.courseName, pdfBlob, options.fileName);
-    await db.put(options.courseName, pdfBlob, options.fileName);
   }
 }
