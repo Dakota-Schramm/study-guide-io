92e86edaa50a69f17269c685b1c498fea623ddff
Dakota Schramm <william.schramm11@gmail.com>
2024-02-10 19:02:18 -0600

    Fix download handlers for course actions

--- src/app/_home/CourseActions.tsx
+++ src/app/_home/CourseActions.tsx
@@ -10,81 +10,18 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Course } from "@/classes/course/course";
 import { UserContext } from "@/contexts/UserContext";
-import { RestrictedAccessUserConfig } from "@/classes/config/user/restricted-access";
 
 const CourseActions = ({ course }: { course: Course }) => {
   const { user } = useContext(UserContext);
-
   const [exams, setExams] = useState<string[][]>([]);
+
   useEffect(() => {
     async function getExams() {
       const exams = await course.getExams();
       setExams(exams);
     }
     getExams();
-  }, []);
-
-  async function handleDownload(exam) {
-    const examFiles = course.files?.filter((file) => exam.includes(file.name));
-
-    if (!examFiles) {
-      alert("No files included in this exam!");
-      return;
-    }
-
-    const pdfFileHandles = examFiles?.filter((file) =>
-      file.name.endsWith(".pdf"),
-    );
-    const pdfFiles = await Promise.all(
-      pdfFileHandles.map(async (file) => await file.getFile()),
-    );
-
-    const attachmentFileHandles = examFiles?.filter(
-      (file) => file.name.endsWith(".png") || file.name.endsWith(".jpg"),
-    );
-    const attachmentFiles = await Promise.all(
-      attachmentFileHandles.map(async (file) => await file.getFile()),
-    );
-
-    if (pdfFiles.length === 0 && attachmentFiles.length === 0) {
-      alert(
-        "Make sure that you're only attachment the following file types to exams: .pdf, .jpg, .png",
-      );
-      return;
-    }
-
-    user.config?.download(pdfFiles, attachmentFiles);
-  }
-
-  async function handleFinalDownload() {
-    const courseFiles = course.getCourseFiles();
-    if (!courseFiles) {
-      alert("No files exist for course");
-    }
-
-    const pdfFileHandles = courseFiles?.filter((file) =>
-      file.name.endsWith(".pdf"),
-    );
-    const pdfFiles = await Promise.all(
-      pdfFileHandles.map(async (file) => await file.getFile()),
-    );
-
-    const attachmentFileHandles = courseFiles?.filter(
-      (file) => file.name.endsWith(".png") || file.name.endsWith(".jpg"),
-    );
-    const attachmentFiles = await Promise.all(
-      attachmentFileHandles.map(async (file) => await file.getFile()),
-    );
-
-    if (pdfFiles.length === 0 && attachmentFiles.length === 0) {
-      alert(
-        "Make sure that you're only attachment the following file types to exams: .pdf, .jpg, .png",
-      );
-      return;
-    }
-
-    new RestrictedAccessUserConfig().downloadGuide(pdfFiles, attachmentFiles);
-  }
+  }, [course]);
 
   return (
     <DropdownMenu>
@@ -97,16 +34,80 @@ const CourseActions = ({ course }: { course: Course }) => {
         <DropdownMenuLabel>Exam Actions</DropdownMenuLabel>
         <DropdownMenuSeparator />
         {exams.map((exam, idx) => (
-          <DropdownMenuItem onClick={() => handleDownload(exam)}>
+          <DropdownMenuItem onClick={() => handleDownload(course, exam)}>
             Download Exam {idx + 1}
           </DropdownMenuItem>
         ))}
-        <DropdownMenuItem onClick={handleFinalDownload}>
+        <DropdownMenuItem
+          onClick={() => handleFinalDownload(user?.config, course)}
+        >
           Download Final Exam
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
 };
+
+async function handleDownload(course, exam) {
+  const examFiles = course.files?.filter((file) => exam.includes(file.name));
+
+  if (!examFiles) {
+    alert("No files included in this exam!");
+    return;
+  }
+
+  const pdfFileHandles = examFiles?.filter((file) =>
+    file.name.endsWith(".pdf"),
+  );
+  const pdfFiles = await Promise.all(
+    pdfFileHandles.map(async (file) => await file.getFile()),
+  );
+
+  const attachmentFileHandles = examFiles?.filter(
+    (file) => file.name.endsWith(".png") || file.name.endsWith(".jpg"),
+  );
+  const attachmentFiles = await Promise.all(
+    attachmentFileHandles.map(async (file) => await file.getFile()),
+  );
+
+  if (pdfFiles.length === 0 && attachmentFiles.length === 0) {
+    alert(
+      "Make sure that you're only attachment the following file types to exams: .pdf, .jpg, .png",
+    );
+    return;
+  }
+
+  user.config?.download(pdfFiles, attachmentFiles);
+}
+
+async function handleFinalDownload(userConfig, course) {
+  const courseFiles = course.getCourseFiles();
+  if (!courseFiles) {
+    alert("No files exist for course");
+  }
+
+  const pdfFileHandles = courseFiles?.filter((file) =>
+    file.name.endsWith(".pdf"),
+  );
+  const pdfFiles = await Promise.all(
+    pdfFileHandles.map(async (file) => await file.getFile()),
+  );
+
+  const attachmentFileHandles = courseFiles?.filter(
+    (file) => file.name.endsWith(".png") || file.name.endsWith(".jpg"),
+  );
+  const attachmentFiles = await Promise.all(
+    attachmentFileHandles.map(async (file) => await file.getFile()),
+  );
+
+  if (pdfFiles.length === 0 && attachmentFiles.length === 0) {
+    alert(
+      "Make sure that you're only attachment the following file types to exams: .pdf, .jpg, .png",
+    );
+    return;
+  }
+
+  userConfig.download(pdfFiles, attachmentFiles);
+}
 
 export default CourseActions;
