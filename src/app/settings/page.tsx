67265be679f2b337d52430c3180d7022176fbb8a
Dakota Schramm <william.schramm11@gmail.com>
2024-02-10 22:36:14 -0600

    Change button styling on settings

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -2,16 +2,23 @@
 
 import React, { useContext } from "react";
 
+import { Button } from "@/components/ui/button";
+
 import { UserContext } from "@/contexts/UserContext";
 
+// TODO: Add button for full user to choose a new home directory
+// TODO: Add button to delete all user data
 export default function SettingsContent() {
   const { reSyncCourses } = useContext(UserContext);
 
   return (
     <>
-      <button type="button" onClick={() => reSyncCourses()}>
-        Setup directory
-      </button>
+      <Button className="flex flex-col" onClick={() => reSyncCourses()}>
+        <span className="font-bold">Setup directory</span>
+        <ul className="opacity-50">
+          <li>Reinitialize your courses as if you're logging in</li>
+        </ul>
+      </Button>
     </>
   );
 }
