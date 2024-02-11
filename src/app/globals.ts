049e997de38f6245fefc21309a880d97b6e8d509
Dakota Schramm <william.schramm11@gmail.com>
2024-02-10 19:21:34 -0600

    Fix globals.ts file to instantiate conditionally server vs client

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -5,9 +5,10 @@ appLogger.setDefaultLevel(log.levels.INFO);
 
 declare global {
   interface Window {
-    log: LogLevelLogger; // TODO fix this to correct type
+    log: log.RootLogger; // TODO fix this to correct type
   }
 }
 
-export type LogLevelLogger = typeof appLogger;
-window.log = appLogger;
+if (typeof window !== "undefined") {
+  window.log = appLogger;
+}
