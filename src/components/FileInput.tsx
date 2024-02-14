f0fb8b0723edcbcefe349e985700b1f3fd3e8034
Dakota Schramm <william.schramm11@gmail.com>
2024-02-13 23:09:25 -0600

    Add file input

---
+++ src/components/FileInput.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useRef } from "react";
+
+type FileInputProps = {
+  fileList: File[];
+  onChange(fileList: FileList | null): void;
+  accept?: string;
+  multiple?: boolean;
+};
+
+const FileInput = ({
+  fileList = [],
+  onChange,
+  ...props
+}: FileInputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      const dataTransfer = new DataTransfer();
+      fileList.forEach((file) => dataTransfer.items.add(file));
+      inputRef.current.files = dataTransfer.files;
+    }
+  }, [fileList]);
+
+  return (
+    <input
+      type="file"
+      ref={inputRef}
+      data-testid="uploader"
+      onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+        onChange(e.target.files);
+      }}
+      {...props}
+    />
+  );
+};
+
+export default FileInput;
