5e89248324ac16ec6b37461bee18b64c33606829
Dakota Schramm <william.schramm11@gmail.com>
2024-02-15 15:10:48 -0600

    Fixup layout for permissions page

--- src/app/permissions/layout.tsx
+++ src/app/permissions/layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const PermissionsLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="flex flex-col w-full h-full justify-center items-between space-y-8 container">
+    <main className="flex flex-col h-full items-between space-y-8 mx-10 items-center justify-center">
       {children}
     </main>
   );
