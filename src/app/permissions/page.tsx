3090aeb7d5b1b9fb24398683d28103916d42da86
Dakota Schramm <william.schramm11@gmail.com>
2024-02-15 14:48:46 -0600

    Cleanup permissions page

--- src/app/permissions/page.tsx
+++ src/app/permissions/page.tsx
@@ -4,38 +4,66 @@ import React, { useContext } from "react";
 
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 import { GoAlertFill } from "react-icons/go";
 import { FaLockOpen } from "react-icons/fa6";
 
 import { UserContext } from "@/contexts/UserContext";
 import { sitePath } from "@/lib/utils";
+import { FullAccessUserConfig } from "@/classes/config/user/full-access";
 
 // TODO: Only show alert if user is fullaccess
-const Permissions = () => {
+
+const FullUserAlert = () => (
+  <Alert className="bg-red-700 text-white relative">
+    <GoAlertFill className="absolute left-0 fill-white text-[26px] " />
+    <AlertTitle>Choosing Your Home Directory</AlertTitle>
+    <AlertDescription>
+      {`Please select either your ${sitePath} folder or the one it's in if setting up for the first time.`}
+    </AlertDescription>
+  </Alert>
+);
+
+const SetupCard = () => {
   const { user, reSyncCourses } = useContext(UserContext);
+
+  return (
+    <Card className="flex flex-col items-center">
+      <CardHeader>
+        <CardTitle className="text-xl text-muted-foreground">
+          We need some things from you to get started...
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div>Placeholder image...</div>
+      </CardContent>
+      <CardFooter>
+        <Button onClick={() => reSyncCourses(user?.config)}>
+          <FaLockOpen className="mr-4" />
+          Setup permissions
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+};
+const Permissions = () => {
+  const { user } = useContext(UserContext);
   if (user.config === null)
     throw new Error("RestrictedAccessUserConfig::PermissionRejected");
 
   return (
     <>
-      <div>Placeholder image...</div>
-      <h2 className="text-xl text-muted-foreground">
-        We need some things from you to get started...
-      </h2>
-
-      <Alert className="bg-red-700 text-white relative">
-        <GoAlertFill className="absolute left-0 fill-white text-[26px] " />
-        <AlertTitle>Choosing Your Home Directory</AlertTitle>
-        <AlertDescription>
-          {`Please select either your ${sitePath} folder or the one it's in.`}
-        </AlertDescription>
-      </Alert>
-
-      <Button onClick={() => reSyncCourses(user?.config)}>
-        <FaLockOpen className="mr-4" />
-        Setup permissions
-      </Button>
+      {user.config instanceof FullAccessUserConfig ? (
+        <FullUserAlert />
+      ) : undefined}
+      <SetupCard />
     </>
   );
 };
