2054a8727c6199924094e75e7a0c39edd12c0208
Dakota Schramm <william.schramm11@gmail.com>
2024-02-15 15:24:36 -0600

    Improve spacing in header

--- src/app/HomeHeader.tsx
+++ src/app/HomeHeader.tsx
@@ -18,20 +18,20 @@ export const HomeHeader = () => {
     <header className="flex justify-between w-screen p-4">
       <nav className="flex p-4 space-x-8 divide-x">
         <Link href="/courses">
-          <span className="flex items-center">
-            <FaHouseChimney />
+          <span className="flex items-center text-lg">
+            <FaHouseChimney className="mr-2" />
             Home
           </span>
         </Link>
         <Link href="/create">
-          <span className="flex items-center">
-            <FaFileCirclePlus />
+          <span className="flex items-center text-lg">
+            <FaFileCirclePlus className="mr-2" />
             Create
           </span>
         </Link>
         <Link href="/settings">
-          <span className="flex items-center">
-            <FaGear />
+          <span className="flex items-center text-lg">
+            <FaGear className="mr-2" />
             Settings
           </span>
         </Link>
