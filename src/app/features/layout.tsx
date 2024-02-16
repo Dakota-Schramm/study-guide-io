29fb47e32fd1f9626ddadd7978ba09e235585600
Dakota Schramm <william.schramm11@gmail.com>
2024-02-15 23:00:47 -0600

    Add start to features pages

---
+++ src/app/features/layout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FeaturesLayout = ({ children }) => {
+  return <main className="container">{children}</main>;
+};
+
+export default FeaturesLayout;
---
+++ src/app/features/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// TODO: Write out pages for SEO that allow users to search for features
+// - Use these to link users and recommend the app based on small features
+const FeaturesPage = () => {
+  return (
+    <>
+      <div>FeaturesPage</div>
+      <p>WIP</p>
+    </>
+  );
+};
+
+export default FeaturesPage;
