427959f8e2c5145b7d83d089bcd14496538a450c
Dakota Schramm <william.schramm11@gmail.com>
2024-02-20 17:54:38 -0600

    Add basic combobox to form

--- src/app/(guides)/create/CourseNameInput.tsx
+++ src/app/(guides)/create/CourseNameInput.tsx
@@ -1,15 +1,41 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { Combobox } from "@headlessui/react";
 
-import TextInput from "./TextInput";
+import { UserContext } from "@/contexts/UserContext";
 
-const CourseNameInput = () => {
+const CourseNameComboBox = () => {
+  const { user } = useContext(UserContext);
+  const [query, setQuery] = useState("");
+
+  const courses = user?.courses ?? [];
+  const filteredCourses =
+    query === ""
+      ? courses
+      : courses.filter((course) => {
+          return course?.getName()?.includes(query.toLowerCase());
+        });
+
   return (
-    <TextInput
-      labelText="Course Name"
-      name="course-name"
-      placeholder="Mathematics"
-    />
+    <Combobox value={filteredCourses}>
+      <Combobox.Input
+        name="course-name"
+        placeholder="Mathematics"
+        onChange={(event) => setQuery(event.target.value)}
+        value={query}
+      />
+      <Combobox.Options>
+        {filteredCourses.map((course) => (
+          <Combobox.Option
+            key={course.id}
+            value={course.getName()}
+            onClick={() => setQuery(course.getName() ?? "")}
+          >
+            {course.getName()}
+          </Combobox.Option>
+        ))}
+      </Combobox.Options>
+    </Combobox>
   );
 };
 
-export default CourseNameInput;
+export default CourseNameComboBox;
